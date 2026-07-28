8bd99e9f94d0ebc10072f7d8248ec0bd972edc14
郑文武 <flamegreat@vip.qq.com>
2026-07-28 12:28:24 +0800

    docs(gallery): 完善控件展示示例

--- samples/ZenUI.Wpf.Gallery/Views/ComboBoxView.xaml.cs
+++ samples/ZenUI.Wpf.Gallery/Views/ComboBoxView.xaml.cs
@@ -1,9 +1,20 @@
+using System;
 using System.Windows.Controls;
 
 namespace ZenUI.Wpf.Gallery.Views
 {
+    public enum OrderStatus
+    {
+        Pending,
+        Processing,
+        Completed,
+        Cancelled
+    }
+
     public partial class ComboBoxView : UserControl
     {
+        public Array OrderStatuses { get; } = Enum.GetValues(typeof(OrderStatus));
+
         public ComboBoxView() { InitializeComponent(); }
     }
 }
